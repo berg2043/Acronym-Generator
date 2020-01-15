e5f247a03eaae5bb479aa8b3229ec2ddb132cfd5
Phillip Berg <phillipb1991@msn.com>
2020-01-15 09:33:30 -0600

    added session middleware and words route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-
+const sessionMiddleware = require('./modules/session');
+const wordRouter = require('./routes/wordRouter');
+require('dotenv').config();
 
 // Globals
 const app = express();
@@ -8,8 +10,10 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(sessionMiddleware);
 
 // Routes
+app.use('/api/words', wordRouter);
 
 // Static
 app.use(express.static('build'));
