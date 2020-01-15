94c3ba5c0b53b914c856ded6f3e3df407d3f8035
Phillip Berg <phillipb1991@msn.com>
2020-01-15 10:31:49 -0600

    set up basic log in

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button } from '@material-ui/core';
+import PasswordInput from '../PasswordInput/PasswordInput';
 
 const LogIn = () => {
+  // States
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return(
     <>
-
+      <div>
+        <form onSubmit={event=>event.preventDefault()}>
+          <TextField
+            label='Username'
+            value={username}
+            onChange={event=>setUsername(event.target.value)}
+          />
+          <br/>
+          <PasswordInput
+            label='Password'
+            value={password}
+            onChange={event=>setPassword(event.target.value)}
+          />
+          <br/>
+          <Button
+            type='submit'
+          >
+            Log In
+          </Button>
+        </form>
+      </div>
     </>
   );
 };
