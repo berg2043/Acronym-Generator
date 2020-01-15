fe2e88dda766d1f5a4753fdc1d41cf7c82e28231
Phillip Berg <phillipb1991@msn.com>
2020-01-15 14:33:27 -0600

    added user login, registeration, and authentication

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const sessionMiddleware = require('./modules/session');
+const passport = require('./strategies/userStrategy'); 
 const wordRouter = require('./routes/wordRouter');
+const userRouter = require('./routes/userRouter');
 require('dotenv').config();
 
 // Globals
@@ -12,8 +14,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(sessionMiddleware);
 
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Routes
 app.use('/api/words', wordRouter);
+app.use('/api/user', userRouter);
 
 // Static
 app.use(express.static('build'));
