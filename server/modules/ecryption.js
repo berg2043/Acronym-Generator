191f5293f73a19dc8e09cd21f0de727fb36bc13e
Phillip Berg <phillipb1991@msn.com>
2020-01-15 14:32:41 -0600

    set up encryption

---
+++ server/modules/ecryption.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcrypt');
+
+// Salt security
+const SALT_WORK_FACTOR = 10;
+
+// Returns a hash and salted password to be stored on DB
+const encryptPassword = (password) => {
+  const salt = bcrypt.genSaltSync(SALT_WORK_FACTOR);
+  return bcrypt.hashSync(password, salt);
+};
+
+// Compares password provided by user to one stored on DB
+const comparePassword = (providedPassword, storedPassword) => {
+  return bcrypt.compareSync(providedPassword, storedPassword);
+};
+
+module.export = {
+  encryptPassword,
+  comparePassword
+}
