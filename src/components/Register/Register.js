0c0440da149e9d2c23d1a80b4235af38720a4108
Phillip Berg <phillipb1991@msn.com>
2020-01-15 15:51:46 -0600

    added registration functionality

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,24 +1,34 @@
 import React, { useState } from 'react';
 import { TextField, Button } from '@material-ui/core';
 import PasswordInput from '../PasswordInput/PasswordInput';
+import { useDispatch } from 'react-redux';
 
 const Register = () => {
+  const dispatch = useDispatch();
+
   // States
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  function registerUser(event){
+    event.preventDefault();
+    dispatch({type: 'REGISTER', payload: {username, password}});
+  }
 
   return(
     <>
       <div>
         <h1>Register</h1>
-        <form onSubmit={event=>event.preventDefault()}>
+        <form onSubmit={registerUser}>
           <TextField
             label='Username'
             value={username}
+            required
             onChange={event=>setUsername(event.target.value)}
           />
           <br/>
           <PasswordInput
+            required
             label='Password'
             value={password}
             onChange={event=>setPassword(event.target.value)}
