990c2014ef71412d8fab0532e87c37230b93f553
Phillip Berg <phillipb1991@msn.com>
2020-01-15 10:38:18 -0600

    added register link

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
 import { TextField, Button } from '@material-ui/core';
 import PasswordInput from '../PasswordInput/PasswordInput';
+import { useHistory } from 'react-router-dom';
 
 const LogIn = () => {
   // States
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-
+  
+  // Adds histroy for progromatic routing
+  const history = useHistory();
+  
   return(
     <>
       <div>
+        <h1>Log In</h1>
         <form onSubmit={event=>event.preventDefault()}>
           <TextField
             label='Username'
@@ -29,6 +34,8 @@ const LogIn = () => {
             Log In
           </Button>
         </form>
+        <p>Not Registered Yet?</p>
+        <p><a href="/register" onClick={event=>{event.preventDefault(); history.push('/register')}}>Register Here</a></p>
       </div>
     </>
   );
