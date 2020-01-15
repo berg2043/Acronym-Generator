7da2cef00dc04ba4994dd78a80e1f58606c797a7
Phillip Berg <phillipb1991@msn.com>
2020-01-15 15:50:53 -0600

    set up username reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
-import acronyms from "./acronyms"
+import acronyms from "./acronyms";
+import username from "./username";
 
 export default combineReducers({
   acronyms,
+  username,
 })
---
+++ src/redux/reducers/username.js
@@ -0,0 +1,10 @@
+export default (state = {}, action) => {
+  switch (action.type) {
+    case 'SET_USER':
+      return action.payload
+    case 'REMOVE_USER':
+      return {};
+    default:
+      return state;
+  }
+};
