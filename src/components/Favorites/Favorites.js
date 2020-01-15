4b0a8f195cadc1d578ea8a8be86b8ff7da4bcee0
Phillip Berg <phillipb1991@msn.com>
2020-01-15 15:52:05 -0600

    set up base favorites page

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Favorites = () => {
+  const user = useSelector(state=>state.username);
+  
   return(
     <>
-
+      <h1>Welcome {user.username}</h1>
     </>
   );
 };
