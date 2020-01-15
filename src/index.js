fb0c70ba9e79eea0d8c07ba89098f9f23f4e9da8
Phillip Berg <phillipb1991@msn.com>
2020-01-15 13:20:32 -0600

    added sagas, reducers, and redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import createSagaMiddleware from 'redux-saga';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './redux/reducers';
+import sagas from './redux/sagas';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  reducers,
+  applyMiddleware(sagaMiddleware,logger)
+)
+
+sagaMiddleware.run(sagas)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
