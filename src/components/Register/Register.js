b86fb741a7dce6049a1b456ee28535f9372be6f6
Phillip Berg <phillipb1991@msn.com>
2020-01-15 10:38:31 -0600

    set up basic registration page

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button } from '@material-ui/core';
+import PasswordInput from '../PasswordInput/PasswordInput';
 
 const Register = () => {
+  // States
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return(
     <>
-
+      <div>
+        <h1>Register</h1>
+        <form onSubmit={event=>event.preventDefault()}>
+          <TextField
+            label='Username'
+            value={username}
+            onChange={event=>setUsername(event.target.value)}
+          />
+          <br/>
+          <PasswordInput
+            label='Password'
+            value={password}
+            onChange={event=>setPassword(event.target.value)}
+          />
+          <br/>
+          <Button
+            type='submit'
+          >
+            Reigster
+          </Button>
+        </form>
+      </div>
     </>
   );
 };
