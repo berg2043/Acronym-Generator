580672472b767d09c0c6da9b24fcb5098bb91f3e
Phillip Berg <phillipb1991@msn.com>
2020-01-17 11:07:17 -0600

    added get and put sagas to favorites

--- src/redux/sagas/favoritesSaga.js
+++ src/redux/sagas/favoritesSaga.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
-import { takeLatest } from "redux-saga/effects";
+import { takeLatest, takeEvery, put } from "redux-saga/effects";
 
 function* addFavorite(action){
   try {
@@ -12,12 +12,33 @@ function* addFavorite(action){
 function* removeFavorite(action){
   try {
     yield Axios.delete('/api/favorites', {data: action.payload})
+    yield put({type: 'GET_FAVORITES'});
   } catch (error) {
     console.log('error deleting favorites', error)
+  }
+}
+
+function* getFavorites(action){
+  try{
+    const favorites = yield Axios.get('/api/favorites');
+    yield put({type: 'SET_FAVORITES', payload: favorites.data});
+  } catch (error) {
+    console.log('error getting favorites', error)
+  }
+}
+
+function* updateFavorite(action){
+  try {
+    yield Axios.put('/api/favorites', action.payload);
+    yield put({type: 'GET_FAVORITES'});
+  } catch (error) {
+    console.log('error updating favorites', error)
   }
 }
 
 export default function* favoritesSaga(){
   yield takeLatest('ADD_FAVORITE', addFavorite);
   yield takeLatest('REMOVE_FAVORITE', removeFavorite);
+  yield takeEvery('GET_FAVORITES', getFavorites);
+  yield takeLatest('UPDATE_FAVORITE', updateFavorite);
 };
