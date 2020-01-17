cc4f4278813497f27f619e56913f006480b874f0
Phillip Berg <phillipb1991@msn.com>
2020-01-17 11:06:06 -0600

    set up favorites display

---
+++ src/components/FavoriteAcronym/FavoriteAcronym.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react';
+import { makeStyles, List, ListItem, ListItemText, Collapse, IconButton, Button, TextField } from '@material-ui/core';
+import { ExpandLess, ExpandMore, Favorite, FavoriteBorder } from '@material-ui/icons/';
+import WordList from '../WordList/WordlList';
+import { useDispatch } from 'react-redux';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
+}));
+
+const FavoriteAcronym = (props) => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const [edit, setEdit] = useState(false);
+  const [words, setWords] = useState([])
+  const [acronym, setAcronym] = useState(props.favorite.word)
+
+  useEffect(()=>{
+    const holder={}
+    props.favorite.word_list.map((word, i)=>{
+      holder[i] = word
+    })
+    setWords(holder)
+  }, [props.favorite.word_list])
+
+  function saveChange(){
+    setEdit(!edit)
+    dispatch({type: 'UPDATE_FAVORITE', payload: {acronym, words, wordList: props.favorite.word_list}})
+  }
+
+  function removeFavorite(){
+    const objectToSend = {
+      [acronym]:{
+        wordLists: [Object.values(words)]
+      }
+    }
+    dispatch({type: 'REMOVE_FAVORITE', payload: objectToSend});
+  }
+
+  return (
+    <>
+      <ListItem>
+        {
+          edit?
+          <TextField value={acronym} onChange={(event)=>setAcronym(event.target.value)}/>:
+          <ListItemText primary={acronym} />
+        }
+      </ListItem>
+        <List component="div" disablePadding>
+          <ListItem className={classes.nested}>
+            {
+              edit?
+                props.favorite.word_list.map((word, i) =>{
+                  return(
+                    <TextField 
+                      key={i}
+                      value={words[i]}
+                      onChange={(event)=>setWords({...words, [i]: event.target.value})}
+                    />
+                  )
+                }):
+                <ListItemText primary={Object.values(words).join(', ')} />
+            }
+            {
+              edit?
+                <Button onClick={()=>saveChange()}>Save</Button>:
+                <Button onClick={()=>setEdit(!edit)}>Edit</Button>
+            }
+            <Button onClick={removeFavorite}>Remove</Button>
+          </ListItem>
+        </List>
+    </>
+  )
+}
+
+export default FavoriteAcronym;
--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { makeStyles, List, ListSubheader } from '@material-ui/core';
+import FavoriteAcronym from './../FavoriteAcronym/FavoriteAcronym';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+    margin: 'auto'
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
+}));
 
 const Favorites = () => {
-  const user = useSelector(state=>state.username);
   
+  const classes = useStyles();
+  const user = useSelector(state=>state.username);
+  const dispatch = useDispatch();
+  const favorites = useSelector(state=>state.favorites)
+
+  useEffect(()=>{
+    dispatch({type: 'GET_FAVORITES'});
+  }, [dispatch])
+
   return(
     <>
       <h1>Welcome {user.username}</h1>
+      <List
+        component='nav'
+        subheader={
+          <ListSubheader component='div'>
+            Words in Order
+          </ListSubheader>
+        }
+        className={classes.root}
+      >
+        {favorites.map(favorite=>{
+          return(
+            <FavoriteAcronym key={favorite.word_list} favorite={favorite} />
+          )
+        })}
+      </List>
     </>
   );
 };
