fc22f693644e6feb7d7e1fe3964b125129250739
Phillip Berg <phillipb1991@msn.com>
2020-01-20 13:26:49 -0600

    set count up correctly to heart all and warned to log in

--- src/components/Acronym/Acronym.js
+++ src/components/Acronym/Acronym.js
@@ -1,43 +1,74 @@
 import React, { useState } from 'react';
 import { List, ListItem, ListItemText, Collapse, IconButton } from '@material-ui/core';
-import { ExpandLess, ExpandMore, Favorite, FavoriteBorder } from '@material-ui/icons/';
+import { ExpandLess, ExpandMore, Favorite, FavoriteBorder, FlagOutlined, Flag } from '@material-ui/icons/';
 import WordList from '../WordList/WordlList';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Acronym = (props) => {
   const dispatch = useDispatch();
   const [open, setOpen] = useState(false);
   const [all, setAll] = useState(false);
   const [count, setCount] = useState(0);
-
+  const [flagged, setFlagged] = useState(false);
+  const user = useSelector(state=>state.username);
+  
   function handleClick() {
     setOpen(!open);
   }
 
-  function favoriteAll() {
-    if(all){
-      dispatch({type: 'REMOVE_FAVORITE', payload: props.acronym});
-    } else {
-      dispatch({type: 'ADD_FAVORITE', payload: props.acronym});
+  function handleFlag() {
+    if(!flagged){
+      dispatch({type: 'FLAG_WORD', payload: props.acronym})
     }
-    setAll(!all);
+    setFlagged(!flagged);
+  }
+
+  function favoriteAll() {
+    if(user.id){
+      if(all){
+        dispatch({type: 'REMOVE_FAVORITE', payload: props.acronym});
+        setCount(0)
+        setAll(false)
+      } else {
+        dispatch({type: 'ADD_FAVORITE', payload: props.acronym});
+        setAll(!all);
+      }
+    } else {
+      alert('you need to login')
+    }
   }
 
   function favoriteSingle(favoriteList, favoriteStatus){
-    const acronymAndList = {
-      [Object.keys(props.acronym)[0]]: {
-        id: props.acronym[Object.keys(props.acronym)[0]].id,
-        wordLists: [favoriteList]
+    if(user.id){
+      const acronymAndList = {
+        [Object.keys(props.acronym)[0]]: {
+          id: props.acronym[Object.keys(props.acronym)[0]].id,
+          wordLists: [favoriteList]
+        }
       }
-    }
-    if(favoriteStatus){
-      dispatch({type: 'REMOVE_FAVORITE', payload: acronymAndList})
+      if(favoriteStatus){
+        dispatch({type: 'REMOVE_FAVORITE', payload: acronymAndList})
+        setCount(count-1);
+      } else {
+        dispatch({type: 'ADD_FAVORITE', payload: acronymAndList});
+        if(count+1 === props.acronym[Object.keys(props.acronym)[0]].wordLists.length){
+          setAll(true);
+        }
+        setCount(count+1);
+      }
     } else {
-      dispatch({type: 'ADD_FAVORITE', payload: acronymAndList});
+      alert('you need to login')
+    }
+  }
+
+  function loginTest(){
+    if(user.id){
+
     }
   }
 
   function counting(increment){
+    console.log(count, increment)
     setCount(count + increment);
   }
 
@@ -45,11 +76,12 @@ const Acronym = (props) => {
     <>
       <ListItem>
         <ListItemText primary={Object.keys(props.acronym)[0]} />
-        {
-          all || count===props.acronym[Object.keys(props.acronym)[0]].length ?
-            <IconButton onClick={favoriteAll}><Favorite /></IconButton> :
-            <IconButton onClick={favoriteAll}><FavoriteBorder /></IconButton>
-        }
+        <IconButton onClick={handleFlag}>
+          {flagged? <Flag/> : <FlagOutlined/>}
+        </IconButton>
+        <IconButton onClick={favoriteAll}>
+          {all ? <Favorite /> :<FavoriteBorder />}
+        </IconButton>
         {
           open ?
             <IconButton onClick={handleClick}><ExpandLess /></IconButton> :
