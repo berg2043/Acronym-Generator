f0c23bfa6068addd07d6a8a431278f28992a0a7f
Phillip Berg <phillipb1991@msn.com>
2020-01-20 16:24:28 -0600

    Styled redundantly

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,53 +1,177 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import { AppBar, Toolbar, IconButton, Menu, MenuItem, Typography, makeStyles, Button, Chip, Avatar } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import FaceRounded from '@material-ui/icons/Face';
+
+// Material-ui styling
+const useStyles = makeStyles(theme=>({
+  menuButton: {
+      marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 const Nav = () =>{
+  const classes = useStyles();
+  // State for menu button
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorE2, setAnchorE2] = React.useState(null);
+
+  // Handles menu opening
+  function handleMenu(event){
+    setAnchorEl(event.currentTarget)
+  }
+  function handleMenu2(event){
+    setAnchorE2(event.currentTarget)
+  }
+  
+  // Handles clicking menu button
+  function handleClose(event, path){
+    setAnchorEl(null)
+    setAnchorE2(null)
+    if(path){
+      history.push(path);
+    }
+  }
+
   const user = useSelector(state=>state.username);
   const dispatch = useDispatch();
   
   const history = useHistory();
   return(
     <nav>
-      <h1>Acronym Generator</h1>
-      <a
-      href='/'
-      onClick={
-        event=>{
-          event.preventDefault();
-          history.push('/');
-        }
-      }
-      >
-        Home
-      </a>
-      <a
-      href='/favorites'
-      onClick={
-        event=>{
-          event.preventDefault();
-          history.push('/favorites');
-        }
-      }
-      >
-        {user.id? 'Favorites':'Login'}
-      </a>
-      {
-        user.admin?
-          <a
-            href='/admin'
-            onClick={
-              event=>{
-                event.preventDefault();
-                history.push('/admin');
-              }
-            }
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton
+            onClick={handleMenu}
+            className={classes.menuButton}
+            color='inherit'
           >
-            Admin
-          </a>:
-          ''
-      }
-      {user.id? <button onClick={() => dispatch({ type: 'LOGOUT' })}>Log Out</button>: null}
+            <MenuIcon/>
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{vertical: 'top', horizontal: 'left'}}
+            keepMounted
+            transformOrigin={{vertical: 'top', horizontal: 'left'}}
+            open={Boolean(anchorEl)}
+            onClose={(event)=>handleClose(event)}
+          >
+            <MenuItem
+              onClick={
+                event=>{
+                  handleClose(event, '/');
+                }
+              }
+            >
+              Home
+            </MenuItem>
+            {
+              user.id?
+                <MenuItem
+                  onClick={
+                    event=>{
+                      handleClose(event, '/favorites');
+                    }
+                  }
+                >
+                  Favorites
+                </MenuItem> :
+                ''
+            }
+            {
+              user.admin?
+                <MenuItem
+                  onClick={
+                    event=>{
+                      handleClose(event, '/admin');
+                    }
+                  }
+                >
+                  Admin
+                </MenuItem> :
+                ''
+            }
+          </Menu>
+          <h1 className={classes.title}>
+            Acronym Generator
+          </h1>
+          {
+            user.id?
+              <>
+                <Chip
+                  avatar={<Avatar><FaceRounded/></Avatar>}
+                  label={user.username}
+                  clickable
+                  color="primary"
+                  onClick={handleMenu2}
+                />
+                <Menu
+                  anchorEl={anchorE2}
+                  anchorOrigin={{vertical: 'bottom', horizontal: 'left'}}
+                  keepMounted
+                  transformOrigin={{vertical: 'bottom', horizontal: 'left'}}
+                  open={Boolean(anchorE2)}
+                  onClose={(event)=>handleClose(event)}
+                >
+                  <MenuItem
+                    onClick={
+                      event=>{
+                        handleClose(event, '/');
+                      }
+                    }
+                  >
+                    Home
+                  </MenuItem>
+                    <MenuItem
+                      onClick={
+                        event=>{
+                          handleClose(event, '/favorites');
+                        }
+                      }
+                    >
+                      Favorites
+                    </MenuItem>
+                  {
+                    user.admin?
+                      <MenuItem
+                        onClick={
+                          event=>{
+                            handleClose(event, '/admin');
+                          }
+                        }
+                      >
+                        Admin
+                      </MenuItem> :
+                      ''
+                  }
+                  {
+                    user.id?
+                      <MenuItem onClick={() => dispatch({ type: 'LOGOUT' })}>
+                        Log Out
+                      </MenuItem>:
+                      null
+                  }
+                </Menu> 
+              </>:
+              <Button
+              color="inherit"
+              onClick={
+                event=>{
+                  handleClose(event, '/favorites');
+                }
+              }
+            >
+              Login
+            </Button>
+          }
+        </Toolbar>
+      </AppBar>
+      
     </nav>
   );
 };
