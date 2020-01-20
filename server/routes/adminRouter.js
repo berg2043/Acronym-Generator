7e43ffe27312097addfd681416e07753fcf21d67
Phillip Berg <phillipb1991@msn.com>
2020-01-20 14:52:50 -0600

    added admin auth and a post and delete route

--- server/routes/adminRouter.js
+++ server/routes/adminRouter.js
@@ -5,26 +5,53 @@ const { rejectUnauthenticated } = require('../modules/authenticationMiddleware')
 const router = express.Router();
 
 // Gets list of words flagged for removal by users
-router.get('/', rejectUnauthenticated, async (req,res)=>{
+router.get('/', rejectUnauthenticated, async (req, res) => {
   try {
     const admin = await pool.query(`SELECT "admin" FROM "user" WHERE "id" = $1;`, [req.session.passport.user]);
-    console.log(admin.rows[0]);
-    // if(!admin){
-    //   res.sendStatus(403);
-    //   break;
-    // }
-    const queryText = `
-      SELECT "w"."id", "w"."word", LENGTH("w"."word") FROM "removals" "r"
-      JOIN "words" w ON "r"."id" = "w"."id"
-      ORDER BY "w"."word";
-    `
-    const removals = await pool.query(queryText);
-    console.log(removals.rows);
-    res.send(removals.rows);
+    if(admin.rows[0].admin){
+      const queryText = `
+        SELECT "w"."id", "w"."word", LENGTH("w"."word") FROM "removals" "r"
+        JOIN "words" w ON "r"."id" = "w"."id"
+        ORDER BY "w"."word";
+      `
+      const removals = await pool.query(queryText);
+      res.send(removals.rows);
+    } else {
+      res.sendStatus(403);
+    }
   } catch (error) {
     res.sendStatus(500);
-    cosnole.log(err);
-  }
+    console.log(err);
+  };
+});
+
+// Adds flagged items
+router.post('/', (req, res) => {
+  console.log(req.body);
+  const queryText = `INSERT INTO "removals" VALUES($1);`;
+  pool.query(queryText, [req.body.id]).then(() => {
+    res.sendStatus(201);
+  }).catch(err => {
+    console.log(err);
+    res.sendStatus(500);
+  });
+});
+
+// Deletes word from removals
+router.delete('/:id', rejectUnauthenticated, async (req, res) => {
+  try {
+    const admin = await pool.query(`SELECT "admin" FROM "user" WHERE "id" = $1;`, [req.session.passport.user]);
+    if(admin.rows[0].admin){
+      const queryText = `DELETE FROM "removals" WHERE "id" = $1;`;
+      await pool.query(queryText, [req.params.id]);
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(403);
+    }
+  } catch (error) {
+    res.sendStatus(500);
+    console.log(err);
+  };
 });
 
 module.exports = router;
