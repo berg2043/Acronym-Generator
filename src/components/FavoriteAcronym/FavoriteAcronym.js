747d457ea0cade6078f03e4f26c0c2f4d613e532
Phillip Berg <phillipb1991@msn.com>
2020-01-21 11:11:42 -0600

    changed colors and button and input layout

--- src/components/FavoriteAcronym/FavoriteAcronym.js
+++ src/components/FavoriteAcronym/FavoriteAcronym.js
@@ -10,6 +10,20 @@ const useStyles = makeStyles(theme => ({
   },
   nested: {
     paddingLeft: theme.spacing(4),
+    backgroundColor: 'lightgray'
+  },
+  buttons: {
+    margin: theme.spacing(1),
+  },
+  inputs: {
+    margin: theme.spacing(1),
+    minWidth: 200,
+    backgroundColor: '#5F5B5B',
+  },
+  inputBlock: {
+    display: 'inline-block',
+    width: 200, 
+    margin: theme.spacing(1),
   },
 }));
 
@@ -49,30 +63,56 @@ const FavoriteAcronym = (props) => {
         {
           edit?
           <TextField value={acronym} onChange={(event)=>setAcronym(event.target.value)}/>:
-          <ListItemText primary={acronym} />
+          <ListItemText style={{color: 'white'}} primary={acronym} />
         }
       </ListItem>
         <List component="div" disablePadding>
           <ListItem className={classes.nested}>
             {
               edit?
-                props.favorite.word_list.map((word, i) =>{
-                  return(
-                    <TextField 
-                      key={i}
-                      value={words[i]}
-                      onChange={(event)=>setWords({...words, [i]: event.target.value})}
-                    />
-                  )
-                }):
+                <span className={classes.inputBlock}>
+                  {props.favorite.word_list.map((word, i) =>{
+                    return(
+                      <TextField 
+                        key={i}
+                        value={words[i]}
+                        onChange={(event)=>setWords({...words, [i]: event.target.value})}
+                        className={classes.inputs}
+                        variant='filled'
+                      />
+                    )
+                  })}
+                </span>:
                 <ListItemText primary={Object.values(words).join(', ')} />
             }
-            {
-              edit?
-                <Button onClick={()=>saveChange()}>Save</Button>:
-                <Button onClick={()=>setEdit(!edit)}>Edit</Button>
-            }
-            <Button onClick={removeFavorite}>Remove</Button>
+            <span style={{minWidth: 193, float: 'right',  display: 'inline-block'}}>
+              {
+                edit?
+                  <Button 
+                    className={classes.buttons} 
+                    style={{backgroundColor: '#388e3c', color: 'white'}} 
+                    color='Success' onClick={()=>saveChange()}
+                  >
+                    Save
+                  </Button>:
+                  <Button 
+                    className={classes.buttons} 
+                    variant='contained' color='primary' 
+                    onClick={()=>setEdit(!edit)}
+                  >
+                    Edit
+                  </Button>
+              }
+              {edit? <br/> : null}
+              <Button 
+                className={classes.buttons} 
+                variant='contained' 
+                style={{backgroundColor: '#741400', color: 'white'}} 
+                onClick={removeFavorite}
+              >
+                Remove
+              </Button>
+            </span>
           </ListItem>
         </List>
     </>
