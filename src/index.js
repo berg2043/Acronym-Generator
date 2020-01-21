93b777b944ea455bcacf47ba8bc870505d952289
Phillip Berg <phillipb1991@msn.com>
2020-01-21 09:21:39 -0600

    changed theme

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import reducers from './redux/reducers';
 import sagas from './redux/sagas';
 import logger from 'redux-logger';
 import { Provider } from 'react-redux';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
+import './index.css';
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -17,4 +19,25 @@ const store = createStore(
 
 sagaMiddleware.run(sagas)
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#023549'
+    },
+    secondary: {
+      main: '#745200'
+    },
+    error: {
+      main: '#741400'
+    },
+    warning: {
+      main: '#B58715'
+    },
+    info: {
+      main: '#115671'
+    },
+  }
+})
+
+ReactDOM.render(<Provider store={store}><MuiThemeProvider theme={theme}><App /></MuiThemeProvider></Provider>, document.getElementById('root'));
