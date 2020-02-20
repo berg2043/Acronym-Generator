f3f41750161655b3c13b47c5cf28fbf71d260879
Phillip Berg <phillipb1991@msn.com>
2020-02-20 00:04:02 -0600

    added likely unecessary /

--- src/redux/sagas/getAcronymsSaga.js
+++ src/redux/sagas/getAcronymsSaga.js
@@ -4,7 +4,7 @@ import { put, takeLatest } from "redux-saga/effects";
 // Gets all the acronymns after the words have been stored in a session
 function* getAcronyms(action){
   try {
-    const results = yield Axios.get('/api/words');
+    const results = yield Axios.get('/api/words/');
     yield put({type: "SET_ACRONYMS", payload: results.data})
   } catch (error) {
     console.log('error getting acronyms', error)
