826e6acb3bb5cb66ccbfe11e8e5c0a1f5a04bc6c
Phillip Berg <phillipb1991@msn.com>
2020-02-24 14:07:14 -0600

    improved navigation bar

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -13,7 +13,7 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const Results = (props) => {
+const Results = () => {
   const classes = useStyles();
   const params = useParams();
   const dispatch = useCallback(useDispatch());
@@ -21,35 +21,45 @@ const Results = (props) => {
   const count = useSelector(state => state.count);
   const [pages, setPages] = useState([]);
   useEffect(() => {
-    dispatch({ type: 'GET_ACRONYMS', payload: params.page -1 });
+    dispatch({ type: 'GET_ACRONYMS', payload: +params.page - 1 });
   }, [params.page, dispatch]);
 
-  useEffect(()=>{
-    const pageArr=[];
-    for(let i = 0; i< count/10; i++){
-      pageArr.push(i+1);
+  useEffect(() => {
+    const pageArr = [];
+    for (let i = 0; i < count / 10; i++) {
+      pageArr.push(i + 1);
     };
     setPages(pageArr);
-  },[count]);
+  }, [count]);
 
   return (
     <div>
-      <List
-        component='nav'
-        subheader={
-          <ListSubheader component='div' style={{ backgroundColor: 'black' }}>
-            Acronyms
-          </ListSubheader>
-        }
-        className={classes.root}
-      >
-        {acronyms.map(acronym => {
-          return (
-            <Acronym acronym={acronym} key={acronym.word_id} />
-          )
-        })}
-      </List>
-      <span>{pages.length > 0 ? pages.map(i=><span><Link to={`/results/${i}`} key={i}>{i}</Link>&nbsp;</span>): null}</span>
+      <div>
+        <List
+          component='nav'
+          subheader={
+            <ListSubheader component='div' style={{ backgroundColor: 'black' }}>
+              Acronyms
+            </ListSubheader>
+          }
+          className={classes.root}
+        >
+          {acronyms.map(acronym => {
+            return (
+              <Acronym acronym={acronym} key={acronym.word_id} />
+            )
+          })}
+        </List>
+      </div>
+      <div style={{ paddingTop: 10, paddingBottom: 20 }}>
+        <span>
+          {+params.page === 1 ? null : <Link to={`${+params.page - 1}`}>&lt;&lt;</Link>}
+          &nbsp;
+          {pages.length > 0 ? pages.map(i => <span><Link to={`/results/${i}`} key={i}>{i}</Link>&nbsp;</span>) : null}
+          &nbsp;
+          {+params.page === Math.ceil(count / 10) ? null : <Link to={`${+params.page + 1}`}>&gt;&gt;</Link>}
+        </span>
+      </div>
     </div>
   )
 }
