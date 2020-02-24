582a97033b1a2eb24c8b15d7af94004aff1d4b39
Phillip Berg <phillipb1991@msn.com>
2020-02-24 13:56:11 -0600

    added pagination to results page

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,17 +1,55 @@
-import React, { useCallback, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { makeStyles, List, ListSubheader } from '@material-ui/core';
+import Acronym from '../Acronym/Acronym';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+    margin: 'auto'
+  },
+}));
 
 const Results = (props) => {
+  const classes = useStyles();
   const params = useParams();
   const dispatch = useCallback(useDispatch());
+  const acronyms = useSelector(state => state.acronyms);
+  const count = useSelector(state => state.count);
+  const [pages, setPages] = useState([]);
+  useEffect(() => {
+    dispatch({ type: 'GET_ACRONYMS', payload: params.page -1 });
+  }, [params.page, dispatch]);
+
   useEffect(()=>{
-    dispatch({type: 'GET_ACRONYMS', payload: params.page});
-  },[params.page, dispatch])
-  return(
+    const pageArr=[];
+    for(let i = 0; i< count/10; i++){
+      pageArr.push(i+1);
+    };
+    setPages(pageArr);
+  },[count]);
+
+  return (
     <div>
-      {JSON.stringify(params.page)}
-      <br/>
+      <List
+        component='nav'
+        subheader={
+          <ListSubheader component='div' style={{ backgroundColor: 'black' }}>
+            Acronyms
+          </ListSubheader>
+        }
+        className={classes.root}
+      >
+        {acronyms.map(acronym => {
+          return (
+            <Acronym acronym={acronym} key={acronym.word_id} />
+          )
+        })}
+      </List>
+      <span>{pages.length > 0 ? pages.map(i=><span><Link to={`/results/${i}`} key={i}>{i}</Link>&nbsp;</span>): null}</span>
     </div>
   )
 }
