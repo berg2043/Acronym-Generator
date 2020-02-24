3f6eddb46da511be015b468204129e7d705227b3
Phillip Berg <phillipb1991@msn.com>
2020-02-24 12:46:27 -0600

    acronyms now gotten when results page loads

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
 const Results = (props) => {
   const params = useParams();
   const dispatch = useCallback(useDispatch());
-  // useEffect(()=>{
-  //   dispatch({type: 'GET_ACRONYMS'});
-  // })
+  useEffect(()=>{
+    dispatch({type: 'GET_ACRONYMS'});
+  },[params.page, dispatch])
   return(
     <div>
       {JSON.stringify(params.page)}
